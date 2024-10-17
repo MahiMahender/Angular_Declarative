0856c19b45a0dae34335b29d7b383d9982d71c1a
Mahender <mahenderktl750@gmail.com>
2024-10-17 15:56:49 +0530

    Video 03 end Commit

--- src/app/Pages/posts/posts.component.ts
+++ src/app/Pages/posts/posts.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/Services/post.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
+    this.postService.getPosts().subscribe((data) => console.log(data));
   }
-
 }
