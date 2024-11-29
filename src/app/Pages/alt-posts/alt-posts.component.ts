6da4813e525d50bb7e004cbbe79a737a0b8d19d3
Mahender <mahenderktl750@gmail.com>
2024-11-29 10:23:00 +0530

    ng13_Share_ActionStream_Between_Components end commit

--- src/app/Pages/alt-posts/alt-posts.component.ts
+++ src/app/Pages/alt-posts/alt-posts.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { IPost } from 'src/app/Models/IPost';
+import { DeclarativePostsService } from 'src/app/Services/declarative-posts.service';
 
 @Component({
   selector: 'app-alt-posts',
   templateUrl: './alt-posts.component.html',
-  styleUrls: ['./alt-posts.component.css']
+  styleUrls: ['./alt-posts.component.css'],
 })
 export class AltPostsComponent implements OnInit {
+  posts$ = this.postsService.postsWithCategory$;
+  constructor(private postsService: DeclarativePostsService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onPostSelect(post: IPost, event: Event) {
+    event.preventDefault();
+    console.log(post);
   }
-
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostsComponent } from './Pages/posts/posts.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { DeclarativePostsComponent } from './Pages/declarative-posts/declarative-posts.component';
+import { AltPostsComponent } from './Pages/alt-posts/alt-posts.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'posts', component: PostsComponent },
   { path: 'declarativePosts', component: DeclarativePostsComponent },
+  { path: 'altPosts', component: AltPostsComponent },
 ];
 
 @NgModule({
